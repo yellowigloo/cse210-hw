2b7ee4dd9c0677659d70a15ba2056424bb42888e
yelloigloo <aaronwilkerson8933@gmail.com>
2023-01-07 22:38:04 -0700

    I did the prep work for Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,38 @@
 using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string letterGrade="";
+        Console.Write("What is your grade percentage? ");
+        int grade = int.Parse(Console.ReadLine());
+        string sign = "";
+        if (grade <93 && grade >=60){
+            if (grade % 10 <3){
+                sign = "-";
+            }
+            else if (grade % 10 >=7){
+                sign = "+";
+            }
+        }
+        if (grade >= 90){
+            letterGrade = "A";
+        }
+        else if (grade >= 80){
+            letterGrade = "B";
+        }
+        else if (grade >= 70){
+            letterGrade = "C";
+        }
+        else if (grade >= 60){
+            letterGrade = "D";
+        }
+        else if (grade < 60){
+            letterGrade = "F";
+        }
+        else {
+            Console.WriteLine("Error");
+        }
+        Console.WriteLine($"You got a {sign}{letterGrade}");
     }
 }
