0e3f6968e1ebfda7c7594fcaaa698fa7f80ac9af
yelloigloo <aaronwilkerson8933@gmail.com>
2023-01-07 22:39:12 -0700

    I did the prep work for Prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string f_name = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string l_name = Console.ReadLine();
+        Console.WriteLine($"Your name is {l_name}, {f_name} {l_name}.");
+        
     }
 }
