dd092d4f10446d552edda0eda3333d8da59a7716
yelloigloo <aaronwilkerson8933@gmail.com>
2023-01-16 22:31:21 -0700

    did all the tasks on prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,26 @@
 using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        int guess;
+        //had to look at the random one
+        Random randomGenerator = new Random();
+        int mgNumber = randomGenerator.Next(1, 10);
+        do{
+            Console.WriteLine("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            if (guess > mgNumber){
+                Console.WriteLine("Lower");
+            }
+            else if (guess < mgNumber){
+                Console.WriteLine("Higher");
+            }
+            else{
+                Console.WriteLine("You guessed it!");
+            }
+        }while (guess != mgNumber);
+
+
     }
 }
